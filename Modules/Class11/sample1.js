77771b89459ea8756a41dd161bd09398183a074f
Mashrur Elahi <elahi@macbook-pro.myfiosgateway.com>
2022-08-06 12:55:49 -0400

    aug6

---
+++ Modules/Class11/sample1.js
@@ -0,0 +1,9 @@
+let siblingNames = ['Deepak', 'Heena', 'Gigi', 'Yuri'];
+
+console.log(siblingNames);
+
+let siblingNamesR = siblingNames.reverse();
+
+console.log(siblingNamesR);
+
+console.log(siblingNames);
